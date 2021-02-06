28de6be78051183a2f0f3becb7e92b941f14bcf5
Vladimir <vladim-filip@yandex.ru>
2021-02-06 23:52:39 +0300

    Functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('error');
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
+
+let num = 20;
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(5, 4));
+// console.log(calc(10, 6));
+// console.log(calc(8, 20));
+
+function ret() {
+    let num = 50;
+    return (num);
 }
 
-const num = 50;
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello!");
+};
+
+logger();
+
+const calc = (a, b) => {
+    console.log(1);
+    return a + b;
+};
