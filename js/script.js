0e449e13de8f9a42bbd78955ccb3fb74250125d8
Vladimir <vladim-filip@yandex.ru>
2021-02-13 14:53:25 +0300

    Functions 2

--- js/script.js
+++ js/script.js
@@ -1,36 +1,7 @@
 "use strict";
 
-// if (4 == 9) {
-//     console.log('Ok!');
-// } else {
-//     console.log('error');
-// }
+const num = 12.5;
+console.log(Math.round(num));
 
-let num = 20;
-
-// function calc(a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(5, 4));
-// console.log(calc(10, 6));
-// console.log(calc(8, 20));
-
-function ret() {
-    let num = 50;
-    return (num);
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log("Hello!");
-};
-
-logger();
-
-const calc = (a, b) => {
-    console.log(1);
-    return a + b;
-};
+const test = "12.2px";
+console.log(parseFloat(test));
