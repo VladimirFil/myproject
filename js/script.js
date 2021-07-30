cb0e096c38a252d5d7f824c00b29639bcdd9e5ef
Vladimir <vladim-filip@yandex.ru>
2021-07-30 19:13:01 +0300

    oop

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ let strObj = new String(str);
 
 console.log(typeof(str));
 console.log(typeof(strObj));
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const john = Object.create(soldier);
+
+/*const john = {
+    health: 100
+};*/
+
+//john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+//console.log(john.armor);
+john.sayHello();
